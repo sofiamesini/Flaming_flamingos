14757779dda57008a13c7b925c1ddf87beabc5e8
ManjodhSingh2020 <manjodh2020@gmail.com>
2024-11-05 14:51:02 +0000

    Adding files for point G, a parameter file and another Transit python file

---
+++ src/daneel/detection/transitVersionForG.py
@@ -0,0 +1,33 @@
+import batman
+import numpy as np
+from matplotlib import pyplot as plt
+import yaml
+import argparse
+
+def transit(PathInput):
+    Labels=["t0","per","rp","a","inc","ecc","w","limb_dark","u"]
+    with open(PathInput,"r") as f:
+        ParamsDict=yaml.safe_load(f)
+    params = batman.TransitParams()       #object to store transit parameters
+    for label in Labels:
+        setattr(params,label,ParamsDict[label])    
+    t = np.linspace(-0.1, 0.1, 2000)  #times at which to calculate light curve
+
+    m = batman.TransitModel(params, t)    #initializes model
+
+    # light curve model
+    flux = m.light_curve(params) #calculates light curve
+    plt.plot(t, flux)
+    plt.xlabel("Time from central transit")
+    plt.ylabel("Relative flux")
+    plt.savefig('WASP-4 b_assignment1_taskF.png')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot the transit light curve.")
+    parser.add_argument("-i", help="Path parameters", required=True, type=str)
+    parser.add_argument("-t", "--transit", help="Generate transit light curve", required=True)
+    args = parser.parse_args()
+
+    if args.transit:
+        transit(args.i)  # Path to the YAML parameters file
+        
