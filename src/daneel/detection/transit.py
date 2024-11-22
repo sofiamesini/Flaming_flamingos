495cd2b3b9004bf711092cf910beed9303270640
sofiamesini <sofia.mesini@gmail.com>
2024-11-22 08:55:56 +0000

    changes in transit.py and renaming of parameters files

--- src/daneel/detection/transit.py
+++ src/daneel/detection/transit.py
@@ -16,15 +16,18 @@ class Transit:
             params = batman.TransitParams()       #object to store transit parameters
             for label in Labels:
                 setattr(params,label,ParamsDict[label])    
-            t = np.linspace(-0.1, 0.1, 2000)  #times at which to calculate light curve
+            t = np.linspace(-3, 3, 30000)  #times at which to calculate light curve
 
-            m = batman.TransitModel(params, t)    #initializes model
-
+            m = batman.TransitModel(params, t, max_err = 0.1)    #initializes model
+            
+            #print(ParamsDict)
             # light curve model
             flux = m.light_curve(params) #calculates light curve
-            plt.plot(t, flux)
+            plt.scatter(t, flux)
             plt.xlabel("Time from central transit [s]")
             plt.ylabel("Relative flux []")
+            #plt.yscale('log')
+            plt.tight_layout()
             Names=Names+" "+ParamsDict["Name"]
         plt.title(Names)
         plt.savefig(Names+'.png')
